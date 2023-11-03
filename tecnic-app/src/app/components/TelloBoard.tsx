95357cb11470c48b2f023f100500b21e839cddca
JOSÉ MARÍA GONZÁLEZ CRUZ <jmaria353@gmail.com>
2023-11-03 01:07:10 -0600

    Update TelloBoard.tsx

--- tecnic-app/src/app/components/TelloBoard.tsx
+++ tecnic-app/src/app/components/TelloBoard.tsx
@@ -22,11 +22,11 @@ const TrelloBoard = () => {
           </div>
           <div className="droppable" onDragEnter={dragEnter} onDragLeave={dragLeave} onDragOver={dragOver} onDrop={drop}>
             <div draggable className="draggable" onDragStart={dragStart}>
-              <p className="p_normal">Mueve la imagen</p>
+              <p className="p_normal">Tarea 1</p>
               <img
                 draggable="false"
                 style={{ width: '100%', height: '100%' }}
-                src="https://images.unsplash.com/photo-1661749701679-639d5e14f4a4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+                src="https://imgs.search.brave.com/6eDSlAH5gmBH0YQDXLmzaOsM7l_6llG9-9XDVtvx2Dw/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4u/ZHJpYmJibGUuY29t/L3VzZXJzLzM5MzYw/MTAvc2NyZWVuc2hv/dHMvNjk5ODU2OS9t/ZWRpYS80ZjMyYzAw/NzhkNjQyOTJmYjNh/NWZhZmM0YTUzNjBm/Yi5qcGc_cmVzaXpl/PTQwMHgzMDAmdmVy/dGljYWw9Y2VudGVy"
                 alt=""
               />
             </div>
@@ -38,13 +38,7 @@ const TrelloBoard = () => {
           </div>
           <div className="droppable" onDragEnter={dragEnter} onDragLeave={dragLeave} onDragOver={dragOver} onDrop={drop}>
             <div draggable className="draggable" onDragStart={dragStart}>
-              <p className="p_normal">Mueve la imagen</p>
-              <img
-                draggable="false"
-                style={{ width: '100%', height: '100%' }}
-                src="https://images.unsplash.com/photo-1661749701679-639d5e14f4a4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
-                alt=""
-              />
+              <p className="p_normal">Tarea 2</p>
             </div>
           </div>
         </div>
@@ -54,11 +48,11 @@ const TrelloBoard = () => {
           </div>
           <div className="droppable" onDragEnter={dragEnter} onDragLeave={dragLeave} onDragOver={dragOver} onDrop={drop}>
           <div draggable className="draggable" onDragStart={dragStart}>
-              <p className="p_normal">Mueve la imagen</p>
+              <p className="p_normal">Tarea 3</p>
               <img
                 draggable="false"
                 style={{ width: '100%', height: '100%' }}
-                src="https://images.unsplash.com/photo-1661749701679-639d5e14f4a4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+                src="https://imgs.search.brave.com/_tYvyHUvABsb7x2LVMxYIVjFRox6OmpEYvufK394OsQ/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/cHNkLWdyYXRpcy9k/aXNlbm8tc2l0aW9z/LXdlYi1zdS1uZWdv/Y2lvXzI0OTcyLTM5/NC5qcGc_c2l6ZT02/MjYmZXh0PWpwZw"
                 alt=""
               />
             </div>
